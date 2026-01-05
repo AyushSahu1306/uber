12d19da705195dcfe994d5ad4f7e9661e3fda616
Ayush Sahu <sahuayush0613@gmail.com>
2026-01-05 20:01:42 +0530

    feat: create hero map component and navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import UserLogin from './pages/UserLogin'
+import CaptainLogin from './pages/CaptainLogin'
+import CaptainRegister from './pages/CaptainRegister'
+import UserRegister from './pages/UserRegister'
+import Layout from './features/global/components/layout'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+        <Route index element={<Home/>}/>
+        <Route path='/user-login' element={<UserLogin/>}/>
+        <Route path='/user-register' element={<UserRegister/>}/>
+        <Route path='/captain-login' element={<CaptainLogin />} />
+        <Route path='/captain-register' element={<CaptainRegister />} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Hero from '@/features/home/components/hero'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='flex flex-row justify-between items-center px-10 w-full mt-20'>
+      <Hero/>
+    </div>
+  )
+}
+
+export default Home
